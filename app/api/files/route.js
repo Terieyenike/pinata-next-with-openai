3915604b46a8209fea9328fbc08f493015889f22
Oteri Eyenike <teyenike1@gmail.com>
2024-10-13 17:08:34 +0100

    File storage project

---
+++ app/api/files/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { pinata } from "@/utils/config"
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export async function POST(request) {
+  try {
+    const data = await request.formData();
+    const file = data.get("file");
+    const { cid } = await pinata.upload.file(file)
+    const url = await pinata.gateways.createSignedURL({
+			cid: cid,
+			expires: 3600,
+		});
+    return NextResponse.json(url, { status: 200 });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
--- app/page.js
+++ app/page.js
@@ -1,101 +1,179 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
+import { preview } from "@/public/assets";
+import { Loader, FormField } from "@/components";
 
-export default function Home() {
+const Create = () => {
+  const [form, setForm] = useState({
+    prompt: "",
+    photo: "",
+  });
+  const { prompt, photo } = form;
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [url, setUrl] = useState("");
+  const [uploading, setUploading] = useState(false);
+
+  const handleChange = (e) => {
+    if (!e || !e.target) {
+      console.error("Event object is not available");
+      return;
+    }
+    const { name, value } = e.target
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSurpriseMe = async () => {
+    try {
+      const response = await fetch(
+        "https://imagen-backend.onrender.com/random-prompt"
+      );
+      if (!response.ok) throw new Error("Failed to fetch prompt");
+
+      const { prompt } = await response.json();
+      setForm({ ...form, prompt });
+    } catch (error) {
+      alert("Error: " + error.message);
+    }
+  };
+
+  const generateImage = async () => {
+    if (prompt) {
+      try {
+        setGeneratingImg(true);
+        setLoading(true); // Set loading state
+
+        const response = await fetch(
+          "https://imagen-backend.onrender.com/api/v1/dalle",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prompt }),
+          }
+        );
+
+        const data = await response.json();
+        setForm({ ...form, photo: data.photo }); // Store the image URL or base64
+      } catch (err) {
+        alert("Error: " + err.message);
+      } finally {
+        setGeneratingImg(false);
+        setLoading(false); // Reset loading state
+      }
+    } else {
+      alert("Please provide a prompt");
+    }
+  };
+
+  const uploadFile = async () => {
+    try {
+      if (!photo) throw new Error("No image generated to upload");
+
+      setUploading(true);
+      const data = new FormData();
+      data.set("file", photo);
+      console.log(data.get('file'), "data set")
+      console.log(data.file, 'this is the data')
+
+      const response = await fetch("/api/files", {
+        method: "POST",
+        body: "https://res.cloudinary.com/terieyenike/image/upload/v1723648082/oteri_eyenike_wvort6.png",
+      });
+
+      // Check if the response is OK, otherwise throw an error
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || "Upload failed");
+      }
+
+      const { signedUrl } = await response.json();
+      setUrl(signedUrl); // Set the signed URL from response
+
+    } catch (e) {
+      console.error("Upload Error:", e);
+      alert(`Error: ${e.message}`);
+    } finally {
+      setUploading(false);
+    }
+  };
+
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
+          Generate an imaginative image through DALL-E AI
+        </p>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+      <div className="mt-16 max-w-3xl flex flex-col gap-5">
+        <FormField
+          labelName="Prompt"
+          type="text"
+          name="prompt"
+          placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
+          value={prompt}
+          handleChange={handleChange}
+          isSurpriseMe
+          handleSurpriseMe={handleSurpriseMe}
+        />
+
+        <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+          {loading ? (
+            <Loader />
+          ) : photo ? (
             <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={photo}
+              alt={prompt}
+              layout="fill"
+              objectFit="contain"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          ) : (
+            <Image
+              width={192}
+              height={192}
+              id="file"
+              src={preview}
+              alt="preview"
+              className="object-contain opacity-40"
+            />
+          )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className="text-white bg-green-700 rounded-md p-2"
+          >
+            {generatingImg ? "Generating..." : "Generate"}
+          </button>
+        </div>
+
+        <div className="mt-5">
+          <button
+            type="button"
+            onClick={uploadFile}
+            disabled={uploading}
+            className="text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {uploading ? "Uploading..." : "Upload Image"}
+          </button>
+          {url && (
+            <a href={url} className="underline" target="_blank" rel="noopener noreferrer">
+              View Uploaded Image
+            </a>
+          )}
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default Create;
